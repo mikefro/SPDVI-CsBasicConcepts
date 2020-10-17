63390def22b97d19fd0b24ffa011754431ea1411
miquelpaucasesnoves <miquelfrontera@paucasesnovescifp.cat>
2020-10-17 22:22:01 +0200

    ForEachListApp and ListModificationChallenge done.
    The first commit about the exercises to practice C# and their grammar and sintaxis language.
    The first exercise show how create a List and loop it for show their contents.
    The second one shows how you can insert a new object in a list in three diferents positions(first,last and middle),
    both in a new list or existing list.

---
+++ ForEachListApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using ForEachListApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace foreachListApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var personaList = new List<PersonModel>();
+
+            personaList.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            personaList.Add(new PersonModel { FirstName = "Bill", LastName = "McCoy" });
+            personaList.Add(new PersonModel { FirstName = "Mary", LastName = "Jones" });
+            personaList.Add(new PersonModel { FirstName = "Sue", LastName = "Smith" });
+
+
+            var alumnes = new List<string>() { "John", "Mary", "Sue", "Greg", "Yolanda",
+             "Jose", "Bill", "Daniel" };
+
+            foreach (var name in alumnes)
+                Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Press any key to continue.....\n\n");
+            Console.ReadKey();
+            Console.WriteLine("List of person from a object list\n");
+
+            foreach (PersonModel persona in personaList)
+                Console.WriteLine($"Hello  {persona.FirstName } {persona.LastName }");
+
+            Console.WriteLine("Press any key to continue.....");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ ListModificationChallengeStarterCode/ListModificationChallenge/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ListModificationChallenge
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
+
+        public string Fullname { get; internal set; }
+    }
+}
